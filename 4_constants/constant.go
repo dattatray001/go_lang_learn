26b715b516d99c39a4f8c1e22a3eca03f10a7879
Dattatray Narhe <dattatrynarhe@gmail.com>
2026-08-03 22:38:45 +0530

    learn: complete Go fundamentals
    - Introduction to Go
    - Why Go
    - Go installation
    - Hello World program
    - Simple values
    - Variables
    - Constants
    - For loops
    - If/Else statements
    - Switch statements

---
+++ 4_constants/constant.go
@@ -0,0 +1,21 @@
+package constants
+
+import "fmt"
+
+// const age = 30
+
+func main() {
+	// :=
+	// const name = "golang"
+	// const age = 30
+
+	// fmt.Println(age)
+
+	const (
+		port = 5000
+		host = "localhost"
+	)
+
+	fmt.Println(port, host)
+
+}
